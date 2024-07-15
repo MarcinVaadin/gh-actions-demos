0928ee9ed5f4965630a25347d7d60a2caf3f4334
marcin <marcin@vaadin.com>
2024-07-15 14:03:20 +0200

    Update watcher.sh

--- scripts/watcher.sh
+++ scripts/watcher.sh
@@ -3,7 +3,7 @@ echo "Monitoring path: $1"
 echo "Dependencies file: $2"
 inotifywait -q -r -e create -e modify $1 -m | while read -r directory action file; do
   echo "$directory $action $file - compiling" >> watcher.log
-  javac -verbose -classpath "`cat $2`:target/classes" $dir/$file -d target/classes > javac.log 2>&1
+  javac -verbose -classpath "`cat $2`:target/classes" $directory/$file -d target/classes > javac.log 2>&1
 done &
 
 echo $! > watcher.pid
